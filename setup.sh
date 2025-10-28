ce2b738a6f93e97349ddc1b40c74b009fce71e3c
jaandaarwall <152979455+jaandaarwall@users.noreply.github.com>
2025-10-28 10:37:02 +0530

    Replace 'uv' with 'python3 -m venv' and 'pip'

--- setup.sh
+++ setup.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-pip install uv
-
 # Optionally create & activate a virtualenv (if preferred)
-uv venv .venv
+python3 -m venv .venv
 source .venv/bin/activate
 
 # Upgrade pip
-uv pip install --upgrade pip
+pip install --upgrade pip
 
 # Install dependencies from your requirements file
-uv pip install -r requirements.txt
-
-# If you have dev/test dependencies:
-
-# Run any database migrations, build steps, etc.
-# e.g. flask db upgrade  (if using Flask-Migrate)
-# or pytest to run tests
+pip install -r requirements.txt
