77e3589227e4852f5613d547582589baadca17c0
jaandaarwall <152979455+jaandaarwall@users.noreply.github.com>
2025-10-28 10:15:52 +0530

    created setup.sh file

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+pip install uv
+
+# Optionally create & activate a virtualenv (if preferred)
+uv venv .venv
+source .venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies from your requirements file
+pip install -r requirements.txt
+
+# If you have dev/test dependencies:
+
+# Run any database migrations, build steps, etc.
+# e.g. flask db upgrade  (if using Flask-Migrate)
+# or pytest to run tests
